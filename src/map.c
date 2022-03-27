57cb8fa2fccb72298804dbbce7dfcaa23b7b974e
Nathanael <sargishovsepyan2001@gmail.com>
2022-03-27 15:03:55 -0400

    added map parsing functions

---
+++ src/map.c
@@ -0,0 +1,54 @@
+#include "../includes/cub3d.h"
+
+static void	process_line(char *line, int *n, int flag)
+{
+	if (flag)
+		ft_exit(4);
+	if (*n < 6)
+		(*n)++;
+	else
+	{
+		if (ft_strlen(line) > map.width)
+			map.width = ft_strlen(line);
+		map.height++;
+		(*n)++;
+	}
+}
+
+void	init_map_size(int fd)
+{
+	int		n;
+	int		flag;
+	char	*line;
+
+	n = 0;
+	flag = 0;
+	while (get_next_line(fd, &line))
+	{
+		if (!is_empty(line))
+			process_line(line, &n, flag);
+		else if (n > 6)
+			flag = 1;
+		free(line);
+	}
+	if (map.gnl)
+		map.height++;
+	free(line);
+}
+
+void	create_map(void)
+{
+	int	i;
+
+	i = 0;
+	map.map = malloc((map.height + 1) * sizeof(char *));
+	if (!map.map)
+		ft_exit(5);
+	while (i < map.height)
+	{
+		map.map[i] = malloc((map.width + 1) * sizeof(char *));
+		if (!map.map[i])
+			ft_exit(5);
+		i++;
+	}
+}

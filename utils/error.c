dda138e41ad7cbd1b36622e03b70f877dd00bc2b
Nathanael <sargishovsepyan2001@gmail.com>
2022-03-27 15:03:38 -0400

    added common error cases

--- utils/error.c
+++ utils/error.c
@@ -3,10 +3,14 @@
 void	ft_exit(int code)
 {
 	if (code == 1)
-		printf("Invalid number of arguments!\n");
+		printf("Error: Invalid number of arguments!\n");
 	else if (code == 2)
-		printf("The input file is invalid. Missing the .cub extension!\n");
+		printf("Error: The input file is missing the .cub extension!\n");
 	else if (code == 3)
-		printf("Failed to open the .cub file!\n");
+		printf("Error: Failed to open the .cub file!\n");
+	else if (code == 4)
+		printf("Error: The input file contains an invalid map!\n");
+	else if (code == 5)
+		printf("Error: Memory allocation is failed!\n");
 	exit(code);
 }

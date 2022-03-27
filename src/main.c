b9a0331e2e283a772be2e5126f4ee2bd37ef86b2
Nathanael <sargishovsepyan2001@gmail.com>
2022-03-27 08:52:22 -0400

    Check the input and start parsing

--- src/main.c
+++ src/main.c
@@ -2,12 +2,15 @@
 
 int	main(int argc, char **argv)
 {
+	win.s_height = 1100;
+	win.s_width = 1280;
 	if (argc != 2)
-	{
-		printf("Invalid number of arguments!\n");
-		exit(1);
-	}
+		ft_exit(1);
 
 	if (!is_valid(argv[1]))
+		ft_exit(2);
+
+	parse(argv[1]);
+	return (0);
 
 }
